59f16230687734bb2d992eb0d7d08f4f00a96bdd
"Vegard Lang&aring <sjefen6@gmail.com>
2011-12-31 01:03:09 +0100

    FFFFFUUUUUU!!!!

--- settings.example.php
+++ settings.example.php
@@ -1,31 +1,3 @@
 <?php
-/*	----------------------------------------
- *	General
- *	--------------------------------------*/
-
-$useSsl = false;
-$defaultContentType = "Jamendo";
-date_default_timezone_set("Europe/Berlin");
-
-/*	----------------------------------------
- *	Features
- *	--------------------------------------*/
-
-$feature_api_icecast = false;
-$feature_api_jamendo = false;
-$feature_api_http_metadata = false;
-
-/*	----------------------------------------
- *	mySQL configuration
- *	--------------------------------------*/
-
-$dbHost= "localhost";
-$dbUser ="nsrmb";
-$dbPass = "goobeligook";
-$dbName = "nsrmb";
-$dbPrefix = "mb_";
-//$dbTable = "";
-
-$connection = mysql_connect($dbHost, $dbUser, $dbPass) or die(mysql_error());
-mysql_select_db($dbName) or die(mysql_error());
+// TODO: Clean settings.php and apply to this file
 ?>
