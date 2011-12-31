e5e6241874b251b78f005d4b69275df9e2aef8c1
"Vegard Lang&aring <sjefen6@gmail.com>
2012-01-01 00:59:57 +0100

    La til http auth og finpuss

--- api/icecast.php
+++ api/icecast.php
@@ -1,7 +1,7 @@
 <?php
-/* 
+/*
  * admin/metadata skal skrives til denne filen
- * 
+ *
  * http://server:port/admin/metadata
  * 		?mount=ignored
  * 		&mode=updinfo
@@ -11,12 +11,27 @@
  * 		&duration=sekunder
  * 		&type=(jamendo,live,promo)
  * Eks:
- * nsrmb.samfunnet.no/beta/admin/metadata?mount=lol&mode=updinfo&song=ignored&artist=WAYDJ&title=CutMeNot%20-%20SSAN&duration=500&type=test
+ * nsrmb.samfunnet.no/admin/metadata?mount=lol&mode=updinfo&song=ignored&artist=WAYDJ&title=CutMeNot%20-%20SSAN&duration=500&type=test
  */
 
- /*
-  * Stuff needed here:
-  */
+/*
+ * Include settings
+ */
+include ("../settings.php");
+
+/*
+ * HTTP authentication
+ */
+if (!(($_SERVER['PHP_AUTH_USER'] == $ice_auth_id) && ($_SERVER['PHP_AUTH_PW'] == $ice_auth_pw))) {
+	header('WWW-Authenticate: Basic realm="Icecast metadata api for NSR-MB"');
+	header('HTTP/1.0 401 Unauthorized');
+	echo 'Authentication failed...';
+	exit ;
+}
+
+/*
+ * Stuff needed here:
+ */
 $mode = $_GET["mode"];
 $artist = $_GET["artist"];
 $title = $_GET["title"];
@@ -32,45 +47,59 @@ $song = $_GET["song"];
 /*
  * Lets check that it contains something usable
  */
-if(($artist == "") && ($title == "") && ($song == ""))
-{
+if (($artist == "") && ($title == "") && ($song == "")) {
 	//This is bs, gtfo
 	echo "Invalid query!";
-	exit;
+	exit ;
 }
 
 /*
  * Not getting separate title and artist
  */
-if(($artist == "") && ($title == ""))
-{
+if (($artist == "") && ($title == "")) {
 	$metadata = explode("-", $song, 2);
-	
+
 	//We assume Artist does not contain "-"
 	$artist = $metadata[0];
 	$title = $metadata[1];
 }
 
 /*
- * 
+ * If we don't get a content type set it to the default
  */
-if ($type == "")
-{
-	
+if ($type == "") {
+	$type = $defaultContentType;
 }
 
 /*
- * Debug info, What info did we get?
- * TODO:Remove!
+ * Check to see duration is set for previous track. If not; assume it endend when this started
  */
-echo "Mode: $mode, Artist: $artist, Title: $title, Duration: $duration, Type: $type";
+$query = "SELECT * FROM library ORDER BY lastPlayed DESC LIMIT 1";
+$result = mysql_query($query) or die(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+	if ($row["duration"] == NULL) {
+		$query = "UPDATE library SET duration = '" . ($now - $row["lastPlayed"]) . "' WHERE sangid = '" . $row["sangid"] . "'";
+		mysql_query($query) or die(mysql_error());
+	}
+}
 
 /*
- * Check if Songs metadata allready exist in db
+ * Check if Songs metadata allready exist in db and update or insert accordingly
  */
-$query = "SELECT * FROM library WHERE artist = $artist AND title = $title";
+$query = "SELECT * FROM library WHERE artist = '$artist' AND title = '$title' LIMIT 1";
+$result = mysql_query($query) or die(mysql_error());
 
-/*
- * Check to see duration is set for previus track. If not; assume it endend when the new started
- */
+if (mysql_num_rows($result) < 1) {
+	if ($duration == "") {
+		$duration = Null;
+	}
+	
+	$query = "INSERT INTO library (artist, title , duration, lastPlayed, type) VALUES('$artist', '$title', '$duration', '$now', '$type')";
+	mysql_query($query) or die(mysql_error());
+} else {
+	while ($row = mysql_fetch_array($result)) {
+		$query = "UPDATE library SET lastPlayed = '$now', playcounter = '" . ($row["playcounter"] + 1) . "' WHERE sangid = '" . $row["sangid"] . "'";
+		mysql_query($query) or die(mysql_error());
+	}
+}
 ?>
