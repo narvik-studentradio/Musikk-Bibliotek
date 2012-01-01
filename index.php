2ebb742c8bdccb5a9be740bdb248180749f48676
"Vegard Lang&aring <sjefen6@gmail.com>
2012-01-01 04:36:59 +0100

    Begynte å jobbe med index.php. Gadd ikke jobbe med css...

--- index.php
+++ index.php
@@ -4,81 +4,71 @@
  * Forsiden. Inneholder ikke noe vital PHP-kode. (Move along)
  *
  */
+include 'settings.php';
 ?>
 
 <html>
 	<head>
-		<title>Narvik Studentradio - Musikkbibliotek (NSR-MB)</title>
-		<style>
-			body {
-				padding: 0px;
-				margin: 0px;
-				font-family: sans-serif;
-				font-size: 12px;
-			}
-			#wrapper {
-				width: 960px;
-				margin: 0px auto;
-			}
-			
-			#header {
-				width: 100%;
-				text-align: center;
-			}
-			
-			#current_box {
-				width: 100%;
-				border: 2px solid black;
-			}
-			
-			#current_box h2 {
-				padding: 0px;
-				margin: 0px;
-				background-color: green;
-				text-align: center;
-				color: white;
-				font-weight: bold;
-			}
-			
-			#current_box  img {
-            float: left;
-            padding: 2px;
-            margin: 5px;
-            width: 100px;
-            height: 100px;
-            border: 1px solid black;
-            background-color: green;
-            }
-            
-            .clear {
-            	clear: both;
-            }
-            
-            p.songtitle {
-            	padding: 0px;
-            	margin: 5px;
-            	font-size: 20px;
-            	font-weight: bold;
-            }
-
-		</style>
+		<title><?php    echo $owner;?>- Musikkbibliotek</title>
+		<link rel="stylesheet" type="text/css" href="browser.css" />
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="header">
-				<h1>Narvik Studentradio: Musikkbibliotek</h1>
-				<h2>Siste oppdatering fra spiller: XX-XX-XX YY:YY:YY</h2>
+				<h1><?php  echo $owner;?>: Musikkbibliotek</h1>
+				<?php $query = "SELECT * FROM library ORDER BY lastPlayed DESC LIMIT 1";
+$result = mysql_query($query) or die(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+				?>
+				<h2>Siste oppdatering fra spiller: <?php  echo date("H:i:s d.m.Y", $row["lastPlayed"]);?></h2>
+				<?php  }?>
 			</div>
+			<?php
+/*
+* Big box for the currently playing song
+*/
+$result = mysql_query($query) or die(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+			?>
 			<div id="current_box">
 				<h2>Spilles nå</h2>
 				<img src="http://imgjam.com/albums/s91/91153/covers/1.200.jpg" />
-				<p class="songtitle">Artist - Title</p>
-				<p class="info">Started: XX:XX</p>
-				<p class="info">Current: XX:XX</p>
-				<p class="info">Duration: XX:XX</p>
+				<p class="songtitle">
+					<?php  echo $row["title"];?><br>
+					<small>Av: <?php  echo $row["artist"];?></small>
+				</p>
+				<p class="info">
+					Started: <?php  echo date("H:i:s", $row["lastPlayed"]);?>
+				</p>
+				<p class="info">
+					Current: <?php  echo date("H:i:s", (time() - $row["lastPlayed"]));?>
+				</p>
+				<p class="info">
+					Duration: <?php  echo date("H:i:s", $row["duration"]);?>
+				</p>
 				<div class="clear"></div>
 			</div>
-			<div id="content"></div>
+			<?php  }?>
+			<?php
+/*
+* Smaller boxes for the previous 4
+*/
+$query = "SELECT * FROM library ORDER BY lastPlayed DESC LIMIT 1, 4";
+$result = mysql_query($query) or die(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+			?>
+			<div class="content">
+				<img src="http://imgjam.com/albums/s91/91153/covers/1.200.jpg" />
+				<p class="info">
+					<?php  echo $row["artist"];?>
+					- <?php  echo $row["title"];?><br>
+					Started: <?php echo date("H:i:s", $row["lastPlayed"]);?><br>
+					Duration: <?php echo date("H:i:s", $row["duration"]);
+					?>
+					<div class="clear"></div>
+			</div>
+			<?php }
+			?>
 		</div>
 	</body>
 </html>
