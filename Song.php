3c03a04de7b1a3bbaa7b0e587ff7690be585dcd7
Vegard Langås <sjefen6@gmail.com>
2012-02-06 00:30:02 +0100

    Decreases the number of db queries from 3 to 1 on index.php

--- Song.php
+++ Song.php
@@ -23,14 +23,12 @@ class Song {
     
     function getXmlItem()
     {
-        $remaining = ($this->lastPlayed + $this->duration - time());
-        $remaining = ($remaining < 1 ? 0 : $remaining);
         return "\t<item>\n"
         	// ENT_XML1 = 16
         . "\t\t<artist>" . htmlspecialchars ($this->artist, 16, "UTF-8") . "</artist>\n"
         . "\t\t<title>" . htmlspecialchars ($this->title, 16, "UTF-8") . "</title>\n"
         . "\t\t<album>" . htmlspecialchars ($this->album, 16, "UTF-8") . "</album>\n"
-        . "\t\t<remaining>" . htmlspecialchars ($remaining, 16, "UTF-8") . "</remaining>\n"
+        . "\t\t<remaining>" . htmlspecialchars (getRemaining(), 16, "UTF-8") . "</remaining>\n"
         . "\t\t<lastPlayed>" . htmlspecialchars (date(r, $this->lastPlayed), 16, "UTF-8") . "</lastPlayed>\n"
         . "\t\t<duration>" . htmlspecialchars ($this->duration, 16, "UTF-8") . "</duration>\n"
         . "\t\t<type>" . htmlspecialchars ($this->type, 16, "UTF-8") . "</type>\n"
@@ -51,4 +49,10 @@ class Song {
             return ($hrs > 0? $hrs . ":" : "") . ($mins > 0 ? $mins . ":" : "") . $secs;
         }
     }
+    
+	public function getRemaining()
+	{
+		$remaining = ($this->lastPlayed + $this->duration - time());
+         return ($remaining < 1 ? 0 : $remaining);
+	}
 }
--- index.php
+++ index.php
@@ -5,65 +5,73 @@
  *
  */
 include 'settings.php';
+include("Song.php");
+
+/*
+ * Lets figure out how many tracks to return
+ */
+$limit = (int) mysql_real_escape_string ($_GET["tracks"]);
+if (($limit > 0) && ($limit <= 100)) {
+	$limit = $limit;
+} else {
+	$limit = 5;
+}
+
+$Songs = array();
+
+/*
+ * Lets query the database get all the Songs
+ */
+
+$query = "SELECT * FROM library ORDER BY lastPlayed DESC LIMIT $limit";
+$result = mysql_query($query) or die(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+	$Songs[] = new Song($row["sangid"], $row["artist"], $row["title"], $row["album"], $row["duration"], $row["lastPlayed"], $row["playcounter"], $row["type"]);
+}
+
+$reloadCountdown = ($Songs[0]->getRemaining() < 10 ? $Songs[0]->getRemaining() : $Songs[0]->getRemaining() + 10);
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title><?php    echo $owner;?>- Musikkbibliotek</title>
 		<link rel="stylesheet" type="text/css" href="browser.css" />
+		<meta http-equiv="refresh" content="<?php echo $$reloadCountdown; ?>">
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="header">
 				<h1><?php  echo $owner;?>: Musikkbibliotek</h1>
-				<?php $query = "SELECT * FROM library ORDER BY lastPlayed DESC LIMIT 1";
-$result = mysql_query($query) or die(mysql_error());
-while ($row = mysql_fetch_array($result)) {
-				?>
-				<h2>Siste oppdatering fra spiller: <?php  echo date("H:i:s d.m.Y", $row["lastPlayed"]);?></h2>
-				<?php  }?>
+				<h2>Siste oppdatering fra spiller: <?php  echo date("H:i:s d.m.Y", $Songs[0]->lastPlayed);?></h2>
 			</div>
-			<?php
-/*
-* Big box for the currently playing song
-*/
-$result = mysql_query($query) or die(mysql_error());
-while ($row = mysql_fetch_array($result)) {
-			?>
+			<!-- Big box for the currently playing song -->
 			<div id="current_box">
-				<h2>Spilles nå</h2>
+				<h2>Spilles n&aring;</h2>
 				<img src="http://imgjam.com/albums/s91/91153/covers/1.200.jpg" />
 				<p class="songtitle">
-					<?php  echo $row["title"];?><br>
-					<small>Av: <?php  echo $row["artist"];?></small>
+					<?php  echo $Songs[0]->title; ?><br>
+					<small>Av: <?php  echo $Songs[0]->artist; ?></small>
 				</p>
 				<p class="info">
-					Started: <?php  echo date("H:i:s", $row["lastPlayed"]);?>
+					Started: <?php  echo date("H:i:s", $Songs[0]->lastPlayed);?>
 				</p>
 				<p class="info">
-					Current: <?php  echo date("H:i:s", (time() - $row["lastPlayed"]));?>
+					Current: <?php  echo date("H:i:s", (time() - $Songs[0]->lastPlayed)); ?>
 				</p>
 				<p class="info">
-					Duration: <?php  echo date("H:i:s", $row["duration"]);?>
+					Duration: <?php  echo date("H:i:s", $Songs[0]->duration); ?>
 				</p>
 				<div class="clear"></div>
 			</div>
-			<?php  }?>
-			<?php
-/*
-* Smaller boxes for the previous 4
-*/
-$query = "SELECT * FROM library ORDER BY lastPlayed DESC LIMIT 1, 4";
-$result = mysql_query($query) or die(mysql_error());
-while ($row = mysql_fetch_array($result)) {
-			?>
+			<!-- Smaller boxes for the previous 4 -->
+			<?php for ($i = 1; $i < 5; $i++) { ?>
 			<div class="content">
 				<img src="http://imgjam.com/albums/s91/91153/covers/1.200.jpg" />
 				<p class="info">
-					<?php  echo $row["artist"];?>
-					- <?php  echo $row["title"];?><br>
-					Started: <?php echo date("H:i:s", $row["lastPlayed"]);?><br>
-					Duration: <?php echo date("H:i:s", $row["duration"]);
+					<?php  echo $Songs[$i]->artist; ?>
+					- <?php  echo $Songs[$i]->title; ?><br>
+					Started: <?php echo date("H:i:s", $Songs[$i]->lastPlayed); ?><br>
+					Duration: <?php echo date("H:i:s", $Songs[$i]->duration);
 					?>
 					<div class="clear"></div>
 			</div>
